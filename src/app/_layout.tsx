9b1bdb641d147c4441e193d56c3478368c2097a2
arthur <arthur.rios007@gmail.com>
2025-09-16 18:08:17 -0300

    Introdução - Configurando React Query

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,13 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Stack } from 'expo-router'
 import '../styles/global.css'
 
+const queryClient = new QueryClient()
+
 export default function RootLayout() {
-  return <Stack />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Stack />
+    </QueryClientProvider>
+  )
 }
