28b2cab918e944d7d0e922afee6981f13ce9e5ab
arthur <arthur.rios007@gmail.com>
2025-09-16 18:02:31 -0300

    Introdução - Configurando Nativewind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,9 @@
-import { router } from 'expo-router'
-import { Text, TouchableOpacity, View } from 'react-native'
+import { Text, View } from 'react-native'
 
 export default function App() {
   return (
     <View>
-      <Text>Olá mundo!</Text>
-      <TouchableOpacity onPress={() => router.push('login')}>
-        <Text>Login</Text>
-      </TouchableOpacity>
+      <Text className="text-purple-base">Olá mundo!</Text>
     </View>
   )
 }
