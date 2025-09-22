3c4346069b891dbb5540f047deb293083a2611cb
arthur <arthur.rios007@gmail.com>
2025-09-22 18:58:31 -0300

    Arquitetura MVVM - Configurando rotas públicas

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -1,9 +1,13 @@
-import { Text, View } from 'react-native'
+import { router } from 'expo-router'
+import { Text, TouchableOpacity, View } from 'react-native'
 
 export default function Login() {
   return (
-    <View>
-      <Text className="">Login</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-purple-base">Login!!</Text>
+      <TouchableOpacity onPress={() => router.push('/register')}>
+        <Text>Registro</Text>
+      </TouchableOpacity>
     </View>
   )
 }
