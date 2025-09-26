ac45b5d5d1a125ee5f7b757259e7b8ebe047347e
arthur <arthur.rios007@gmail.com>
2025-09-26 13:52:55 -0300

    Arquitetura MVVM - Testando registro e configurando endpoint dinâmico da api

--- src/viewModels/Register/useRegister.viewModel.ts
+++ src/viewModels/Register/useRegister.viewModel.ts
@@ -1,8 +1,11 @@
 import { yupResolver } from '@hookform/resolvers/yup'
 import { useForm } from 'react-hook-form'
+import { useRegisterMutation } from '../../shared/queries/auth/use-register.mutation'
 import { RegisterFormData, registerScheme } from './register.scheme'
 
 export const useRegisterViewModel = () => {
+  const userRegisterMutation = useRegisterMutation()
+
   const {
     control,
     handleSubmit,
@@ -10,15 +13,20 @@ export const useRegisterViewModel = () => {
   } = useForm<RegisterFormData>({
     resolver: yupResolver(registerScheme),
     defaultValues: {
-      name: '',
-      email: '',
-      password: '',
-      confirmPassword: '',
-      phone: '',
+      name: 'teste',
+      email: 'teste@gmail.com',
+      password: '123123123',
+      confirmPassword: '123123123',
+      phone: '11111111111',
     },
   })
 
-  const onSubmit = handleSubmit(({}) => {})
+  const onSubmit = handleSubmit(async (userData) => {
+    console.log(userData)
+    const { confirmPassword, ...registerData } = userData
+
+    await userRegisterMutation.mutateAsync(registerData)
+  })
 
   return {
     control,
