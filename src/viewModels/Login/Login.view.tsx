6169bd3324a7e2bf2a94aca130b67c50ec7c53b0
arthur <arthur.rios007@gmail.com>
2025-10-03 15:57:38 -0300

    Tela de registro e login - Criando KeyboardAvoidingView

--- src/viewModels/Login/Login.view.tsx
+++ src/viewModels/Login/Login.view.tsx
@@ -2,20 +2,23 @@ import { router } from 'expo-router'
 import { Text, TouchableOpacity, View } from 'react-native'
 import { AppInput } from '../../shared/components/AppInput'
 import { AuthFormHeader } from '../../shared/components/AuthFormHeader'
+import { KeyboardContainer } from '../../shared/components/KeyboardContainer'
 
 export const LoginView = () => {
   return (
-    <View className="items-center justify-center flex-1">
-      <AuthFormHeader
-        subTitle="Informe seu e-mail e senha"
-        title="Acesse sua conta"
-      />
+    <KeyboardContainer>
+      <View className="items-center justify-center flex-1 px-[40px]">
+        <AuthFormHeader
+          subTitle="Informe seu e-mail e senha"
+          title="Acesse sua conta"
+        />
 
-      <AppInput />
+        <AppInput />
 
-      <TouchableOpacity onPress={() => router.push('/register')}>
-        <Text>Registro</Text>
-      </TouchableOpacity>
-    </View>
+        <TouchableOpacity onPress={() => router.push('/register')}>
+          <Text>Registro</Text>
+        </TouchableOpacity>
+      </View>
+    </KeyboardContainer>
   )
 }
--- src/viewModels/Register/Register.view.tsx
+++ src/viewModels/Register/Register.view.tsx
@@ -1,8 +1,9 @@
 import { router } from 'expo-router'
 import { FC } from 'react'
-import { Text, TouchableOpacity, View } from 'react-native'
+import { ScrollView, Text, TouchableOpacity, View } from 'react-native'
 import { AppInputController } from '../../shared/components/AppInputController'
 import { AuthFormHeader } from '../../shared/components/AuthFormHeader'
+import { KeyboardContainer } from '../../shared/components/KeyboardContainer'
 import { useRegisterViewModel } from './useRegister.viewModel'
 
 export const RegisterView: FC<ReturnType<typeof useRegisterViewModel>> = ({
@@ -10,25 +11,56 @@ export const RegisterView: FC<ReturnType<typeof useRegisterViewModel>> = ({
   control,
 }) => {
   return (
-    <View className="flex-1 justify-center">
-      <AuthFormHeader
-        title="Crie sua conta"
-        subTitle="Informe seus dados pessoais e de acesso"
-      />
-      <AppInputController
-        leftIcon="mail-outline"
-        label="E-MAIL"
-        control={control}
-        name="email"
-      />
+    <KeyboardContainer>
+      <ScrollView className="flex-1 px-[40px]">
+        <AuthFormHeader
+          title="Crie sua conta"
+          subTitle="Informe seus dados pessoais e de acesso"
+        />
+        <AppInputController
+          leftIcon="person-outline"
+          label="NOME"
+          control={control}
+          name="name"
+        />
 
-      <TouchableOpacity onPress={onSubmit}>
-        <Text>Registrar</Text>
-      </TouchableOpacity>
+        <AppInputController
+          leftIcon="call-outline"
+          label="TELEFONE"
+          control={control}
+          name="phone"
+        />
+        <AppInputController
+          leftIcon="mail-outline"
+          label="E-MAIL"
+          control={control}
+          name="email"
+        />
 
-      <TouchableOpacity onPress={() => router.push('/login')}>
-        <Text>Login</Text>
-      </TouchableOpacity>
-    </View>
+        <AppInputController
+          leftIcon="lock-closed-outline"
+          label="SENHA"
+          control={control}
+          name="password"
+          secureTextEntry
+        />
+
+        <AppInputController
+          leftIcon="lock-closed-outline"
+          label="CONFIRMAR SENHA"
+          control={control}
+          name="confirmPassword"
+          secureTextEntry
+        />
+
+        <TouchableOpacity onPress={onSubmit}>
+          <Text>Registrar</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => router.push('/login')}>
+          <Text>Login</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </KeyboardContainer>
   )
 }
