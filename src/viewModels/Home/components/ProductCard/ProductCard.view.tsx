50b4d7f7867333f5623f872cd7189a5a149c8756
arthur <arthur.rios007@gmail.com>
2025-10-29 16:28:37 -0300

    Tela de Home - Estilização base do card do produto

--- src/viewModels/Home/components/ProductCard/ProductCard.view.tsx
+++ src/viewModels/Home/components/ProductCard/ProductCard.view.tsx
@@ -1,14 +1,36 @@
+import { Ionicons } from '@expo/vector-icons'
 import { FC } from 'react'
-import { Text, View } from 'react-native'
+import { Image, Text, TouchableOpacity, View } from 'react-native'
 import { ProductInterface } from '../../../../shared/interfaces/product'
+import { colors } from '../../../../styles/colors'
 import { useProductCardViewModel } from './useProductCard.viewModel'
 
 export const ProductCardView: FC<
   ReturnType<typeof useProductCardViewModel>
 > = ({ product }) => {
   return (
-    <View>
-      <Text>{product.name}</Text>
-    </View>
+    <TouchableOpacity className="w-[48%] my-1 rounded-xl shadow-sm overflow-hidden h-[157px] p-[4px] bg-white mb-2">
+      <View>
+        <Image
+          source={{ uri: product.photo }}
+          className="w-full h-[96px] rounded-md"
+          resizeMode="cover"
+        />
+        <View className="absolute top-0 right-0 flex-row items-center px-2 py-1 rounded-b-lg rounded-r-none bg-white">
+          <Ionicons name="star" size={12} color={colors['blue-base']} />
+          <Text className="text-sm font-semibold ml-1">
+            {product.ratingCount}
+          </Text>
+        </View>
+      </View>
+      <View className="p-3">
+        <Text className="text-xs font-semibold mb-1" numberOfLines={1}>
+          {product.name}
+        </Text>
+        <View className="flex-row items-center justify-between">
+          <Text>R$ {product.value}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
   )
 }
