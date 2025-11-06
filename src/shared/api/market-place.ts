ea8afa6d1c45a5bc88dba979ec1fc2f1d2982f4a
arthur <arthur.rios007@gmail.com>
2025-11-06 19:23:10 -0300

    Revalidando token - Realizando revalidação de token

--- src/shared/api/market-place.ts
+++ src/shared/api/market-place.ts
@@ -37,8 +37,6 @@ export class MarketPlaceApiClient {
             state: { token },
           } = JSON.parse(userData)
 
-          console.log(token)
-
           if (token) {
             config.headers.Authorization = `Bearer ${token}`
           }
@@ -48,6 +46,70 @@ export class MarketPlaceApiClient {
       },
       (error) => {
         return Promise.reject(error)
+      },
+    )
+
+    this.instance.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        const originalRequest = error.config
+
+        if (
+          error.response?.status === 401 &&
+          error.response?.data?.message === 'Token expirado' &&
+          !this.isRefreshing
+        ) {
+          this.isRefreshing = true
+
+          try {
+            const userData = await AsyncStorage.getItem('marketplace-auth')
+
+            if (!userData) {
+              throw new Error('Usuário não autenticado')
+            }
+
+            const {
+              state: { refreshToken },
+            } = JSON.parse(userData)
+
+            if (!refreshToken) {
+              throw new Error('Refresh token não encontrado')
+            }
+
+            const { data: response } = await this.instance.post(
+              '/auth/refresh',
+              {
+                refreshToken,
+              },
+            )
+
+            const currentUserData = JSON.parse(userData)
+
+            currentUserData.state.token = response.token
+            currentUserData.state.refreshToken = response.refreshToken
+
+            await AsyncStorage.setItem(
+              'marketplace-auth',
+              JSON.stringify(currentUserData),
+            )
+
+            originalRequest.headers.Authorization = `Bearer ${response.token}`
+
+            return this.instance(originalRequest)
+          } catch {
+            return Promise.reject(
+              new Error('Sessão expirada, faça o login novamente.'),
+            )
+          } finally {
+            this.isRefreshing = false
+          }
+        }
+
+        if (error.response && error.response.data) {
+          return Promise.reject(new Error(error.response.data.message))
+        } else {
+          return Promise.reject(new Error('Falha na requisição'))
+        }
       },
     )
   }
