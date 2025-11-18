1d0d82071665479fc6af77e2fd2185b1fff6384c
arthur <arthur.rios007@gmail.com>
2025-11-18 20:56:32 -0300

    Tela de pedidos - Formatando dados recebidos

--- src/viewModels/Orders/components/OrderItem/index.tsx
+++ src/viewModels/Orders/components/OrderItem/index.tsx
@@ -1,5 +1,7 @@
+import { format } from 'date-fns'
 import { FC } from 'react'
 import { Image, Text, View } from 'react-native'
+import { AppPriceText } from '../../../../shared/components/AppPriceText'
 import { buildImageUrl } from '../../../../shared/helpers/buildImageUrl'
 import { OrderInterface } from '../../../../shared/interfaces/order'
 
@@ -9,15 +11,15 @@ interface OrderItemParams {
 
 export const OrderItem: FC<OrderItemParams> = ({ order }) => {
   return (
-    <View className="flex-row items-center bg-white p-1 mb-3 rounded-lg">
+    <View className="flex-row items-center bg-white p-3 mb-3 rounded-lg">
       <Image
         source={{ uri: buildImageUrl(order.productPhoto) }}
         className="h-[81px] w-[88px] rounded-lg mr-4"
         resizeMode="cover"
       />
 
-      <View className="flex-1 justify-between py-4">
-        <View className="flex-row justify-between items-start mb-2">
+      <View className="flex-1 justify-between">
+        <View className="flex-row justify-between items-start mb-6 gap-2">
           <Text
             className="text-sm font-semibold text-gray-900 flex-1"
             numberOfLines={1}
@@ -25,13 +27,23 @@ export const OrderItem: FC<OrderItemParams> = ({ order }) => {
             {order.productName}
           </Text>
 
-          <Text>{order.createdAt.toString()}</Text>
+          <Text className="text-sm text-gray-600">
+            {format(order.createdAt, 'dd/MM/yyyy')}
+          </Text>
         </View>
-
-        <Text>
-          {order.quantity} - {order.totalPrice}
+        <View className="flex-row items-center mb-1">
+          <Text className="text-sm text-gray-600 mr-1">
+            {order.quantity} {order.quantity > 1 ? 'Unidades' : 'Unidade'} •{' '}
+          </Text>
+          <AppPriceText
+            value={order.totalPrice}
+            classNameCurrency="text-sm text-gray-600"
+            classNameValue="text-sm text-gray-600"
+          />
+        </View>
+        <Text className="text-sm text-gray-600">
+          Cartão final {order.creditCard.maskedNumber.slice(-4)}
         </Text>
-        <Text>{order.creditCard.maskedNumber}</Text>
       </View>
     </View>
   )
