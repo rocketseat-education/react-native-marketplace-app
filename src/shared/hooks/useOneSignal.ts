3dc0beff400aabcca3507460cd0cc62200c920d6
arthur <arthur.rios007@gmail.com>
2026-01-27 08:15:26 -0300

    Push Notifications - Recebendo notificações

--- src/shared/hooks/useOneSignal.ts
+++ src/shared/hooks/useOneSignal.ts
@@ -6,6 +6,8 @@ const ONESIGNAL_APP_ID = process.env.EXPO_PUBLIC_ONESIGNAL_APP_ID
 export const useOneSignal = () => {
   const [playerId, setPlayerId] = useState<string | undefined>()
 
+  console.log({ playerId })
+
   useEffect(() => {
     if (!ONESIGNAL_APP_ID) return
     OneSignal.initialize(ONESIGNAL_APP_ID)
